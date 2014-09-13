550a8b872cf6b4c98dc2f780ac3ad29eac6653e5
Christian Jaeger <ch@christianjaeger.ch>
2014-09-13 12:27:12 +0100

    make it work transparently without sendfile

--- lib/PFLANZE/copy.pm
+++ lib/PFLANZE/copy.pm
@@ -9,10 +9,14 @@ PFLANZE::copy
 
 =head1 SYNOPSIS
 
+ use PFLANZE::copy 'copy_fast';
+
+ copy_fast $from, $to, $file_size; # dies on errors
+
 =head1 DESCRIPTION
 
-Fast file copy using sendfile. Uses sendfile on Linux >= 2.6.33;
-actually just uses that right now and will fail otherwise.
+Fast file copy using sendfile. Uses sendfile on Linux >= 2.6.33, falls
+back to File::Copy otherwise.
 
 =cut
 
@@ -25,23 +29,62 @@ package PFLANZE::copy;
 
 use strict; use warnings FATAL => 'uninitialized';
 
-use Sys::Syscall ":sendfile";
+sub copy_fast ($$$);
 
-sub copy_fast ($$$) {
-    my ($from, $to, $size)=@_;
-    open my $in, "<", $from
-      or die "open '$from': $!";
-    open my $out, ">", $to
-      or die "open '$to': $!";
-    my $res= sendfile (fileno($out), fileno($in), $size);
-    if ($res < 0) {
-	die "sendfile to '$to': $!";
-    } elsif ($res != $size) {
-	die "sendfile to '$to': transferred $res instead of $size bytes ($!)";
+BEGIN {
+    my $verbose= $ENV{VERBOSE};
+
+    my $impl_sendfile= sub {
+	require Sys::Syscall;
+	*copy_fast= sub ($$$) {
+	    my ($from, $to, $size)=@_;
+	    open my $in, "<", $from
+		or die "open '$from': $!";
+	    open my $out, ">", $to
+		or die "open '$to': $!";
+	    my $res= Sys::Syscall::sendfile (fileno($out), fileno($in), $size);
+	    if ($res < 0) {
+		die "sendfile to '$to': $!";
+	    } elsif ($res != $size) {
+		die "sendfile to '$to': transferred $res instead of $size bytes ($!)";
+	    }
+	    close $in or die "close while copying from '$from': $!";
+	    close $out or die "close while copying to '$to': $!";
+	};
+    };
+    my $impl_fallback= sub {
+	require File::Copy;
+	*copy_fast= sub ($$$) {
+	    my ($from, $to, $size)=@_;
+	    File::Copy::copy($from,$to)
+		or die "copying '$from', '$to': $!";
+	};
+    };
+
+    if (`uname -s` =~ /^Linux/) {
+	my $revision= `uname -r`; # something like "2.6.32-5-686-bigmem"
+	my ($major,$minor,$patch)=
+	    $revision=~ /^([0-9]+)\.([0-9]+)\.([0-9]+)/
+	    or die "no match for '$revision'";
+	if ($major > 2
+	    or
+	    ($major == 2 and
+	     ($minor > 6
+	      or
+	      ($minor == 6 and $patch >= 33)))) {
+	    warn "using sendfile for speed"
+		if $verbose;
+	    &$impl_sendfile;
+	} else {
+	    warn "Linux version too old, falling back to File::Copy"
+		if $verbose;
+	    &$impl_fallback;
+	}
+    } else {
+	warn "not on Linux, falling back to File::Copy"
+	    if $verbose;
+	&$impl_fallback;
     }
-    close $in or die "close while copying from '$from': $!";
-    close $out or die "close while copying to '$to': $!";
 }
-
 
 1
